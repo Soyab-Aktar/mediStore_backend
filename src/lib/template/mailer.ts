eff02fa88dd4642a7aa15547cfe23f1cbf4faeb6
Soyab-Aktar <iamsoyabcode23@gmail.com>
2026-03-14 22:41:39 +0530

    Some Fixes on NodeMailer

--- src/lib/template/mailer.ts
+++ src/lib/template/mailer.ts
@@ -2,13 +2,18 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_ID,
     pass: process.env.GMAIL_PASSWORD,
   },
+  connectionTimeout: 10000,
 });
+
+transporter.verify()
+  .then(() => console.log("SMTP server ready"))
+  .catch(err => console.error("SMTP error:", err));
 
 interface SendEmailParams {
   to: string;
